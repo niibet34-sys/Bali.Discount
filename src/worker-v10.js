17593a6fb2d516443be868467ce4eb5c0ff63ed3
niibet34-sys <niibet34@gmail.com>
2026-08-23 14:50:00 -0700

    Add isolated stable Notion preview routes

--- src/worker-v10.js
+++ src/worker-v10.js
@@ -1,9 +1,16 @@
 import defaultWorker from "./worker-v4.js";
 import englishWorker from "./worker-en-v2.js";
 import russianWorker from "./worker-ru-enstyle.js";
+import stableRenderer from "./worker-static-renderer.js";
 
 export default {
   async fetch(request, env, ctx) {
+    const url = new URL(request.url);
+
+    if (isBranch(url.pathname, "/stable/ru") || isBranch(url.pathname, "/stable/en")) {
+      return stableRenderer.fetch(request, env, ctx);
+    }
+
     const branch = detectBranch(request);
 
     if (branch === "ru") {
